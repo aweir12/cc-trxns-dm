fb1578158ad85217e25f0cf2e8f648edad1c2c29
Austin Weir <Austin.Weir@nblenergy.com>
2018-02-22 15:39:51 -0600

    Hierarchy Snapshot Woes

---
+++ ps-01-month_dim.sql
@@ -0,0 +1,12 @@
+CREATE TABLE MONTH_DIM
+    AS
+        ( SELECT
+            TO_CHAR(LAST_DAY(ADD_MONTHS(SYSDATE,-ROWNUM + 1) ),'YYYYMM') MONTH_WID,
+            LAST_DAY(ADD_MONTHS(SYSDATE,-ROWNUM + 1) ) MONTH
+          FROM
+            DUAL
+        CONNECT BY
+            LEVEL <= 36
+        )
+        ORDER BY
+            1 ASC;
---
+++ ps-04-build_hier_snap.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE PROCEDURE BUILD_RT_HIER_SNAP IS
+BEGIN FOR DT IN (
+    SELECT
+        MONTH_WID,
+        MONTH
+    FROM
+        MONTH_DIM
+)
+LOOP
+    INSERT INTO RT_HIER_SNAP (
+        MONTH_WID,
+        EMPLOYEE_NUMBER,
+        FIRST_NAME,
+        LAST_NAME,
+        EMAIL_ADDRESS,
+        NAME,
+        SUPERVISOR_ID,
+        REPORTS_TO_PATH,
+        EXEC_MANAGER
+    )
+        SELECT
+            DT.MONTH_WID,
+            EMPLOYEE_NUMBER,
+            FIRST_NAME,
+            LAST_NAME,
+            EMAIL_ADDRESS,
+            NAME,
+            SUPERVISOR_ID,
+            REPORTS_TO_PATH,
+            EXEC_MANAGER
+        FROM
+            FUNC_RT_HIER_TABLE_TYPE(DT.MONTH); 
+END LOOP; 
+END BUILD_RT_HIER_SNAP;
+/
+
+EXEC BUILD_RT_HIER_SNAP;
+
